b1213c7a46973ff06a9a4f95c022f8c244d82cbc
suhailaabib <suhailaorbaabib@gmail.com>
2024-07-03 17:28:50 -0300

    commit inicial

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 
-const prompt = require('prompt-sync');
+const prompt = require('prompt-sync')();
 
 let primeiroNumero = prompt ("Digite o primeiro número: ")
 let operador = prompt (" Digite o operador: ")
 let segundoNumero = prompt ("Digite o segundo número: ")
 
+primeiroNumero = parseFloat(primeiroNumero)
+segundoNumero = parseFloat (segundoNumero)
 
 function calculadora (primeiroNumero, segundoNumero, operador){
 
     switch(operador){
    
        case "+":
-         return (primeiroNumero + segundoNumero)
+         return primeiroNumero + segundoNumero
        
        case "-" :
-          return (primeiroNumero - segundoNumero)
+          return primeiroNumero - segundoNumero
    
        case "*":
-        return (primeiroNumero * segundoNumero)
+        return primeiroNumero * segundoNumero
     
        case "/" :
-        return (primeiroNumero / segundoNumero)
+        return primeiroNumero / segundoNumero
 
        case "%" :
-        return (primeiroNumero % segundoNumero)
+        return (primeiroNumero * segundoNumero)/100
        
        default: 
          console.log('Não reconhecido')
@@ -34,3 +36,4 @@ function calculadora (primeiroNumero, segundoNumero, operador){
     
     let resultado = calculadora(primeiroNumero, segundoNumero, operador)
     console.log(`O resultado do calculo é ${resultado}`) 
+    
